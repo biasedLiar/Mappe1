8f4c4cba4674b6b0a1cc82e229f71df231a5f6b9
Elias Baird <eliaseb@stud.ntnu.no>
2021-02-22 12:30:37 +0100

    Siste oppdatert helloworld

--- src/main/java/no/ntnu/eliaseb/HospitalClient.java
+++ src/main/java/no/ntnu/eliaseb/HospitalClient.java
@@ -3,6 +3,6 @@ package no.ntnu.eliaseb;
 public class HospitalClient {
     public static void main(String[] args) {
 
-        System.out.println("Hello Worldtest");
+        System.out.println("Hello Worldtest Siste sjekk");
     }
 }
