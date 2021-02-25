bf38df1aaabef43e1dac8ad94d755e6dac611261
Elias Baird <eliaseb@stud.ntnu.no>
2021-02-25 13:40:47 +0100

    employee og doctor klasse lagt til.

---
+++ src/main/java/no/ntnu/eliaseb/Doctor.java
@@ -0,0 +1,28 @@
+package no.ntnu.eliaseb;
+
+public class Doctor extends Employee{
+    public Doctor(String firstName, String lastName, String socialSecurityNumber) {
+        super(firstName, lastName, socialSecurityNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", socialSecurityNumber='" + socialSecurityNumber + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
---
+++ src/main/java/no/ntnu/eliaseb/Employee.java
@@ -0,0 +1,28 @@
+package no.ntnu.eliaseb;
+
+public abstract class Employee extends Person{
+    public Employee(String firstName, String lastName, String socialSecurityNumber) {
+        super(firstName, lastName, socialSecurityNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", socialSecurityNumber='" + socialSecurityNumber + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
