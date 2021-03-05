86e68069fbf2f7d5b179ef73ebeb9357a0153c60
Elias Baird <eliaseb@stud.ntnu.no>
2021-03-05 22:14:04 +0100

    rettet skrivefeil i GeneralPractitioner

--- src/main/java/no/ntnu/eliaseb/GeneralPractitioner.java
+++ src/main/java/no/ntnu/eliaseb/GeneralPractitioner.java
@@ -20,7 +20,7 @@ public class GeneralPractitioner extends Doctor{
         return "GeneralPractitioner{" +
                 "firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", socialSecuritsdfyNumber='" + socialSecurityNumber + '\'' +
+                ", socialSecurityNumber='" + socialSecurityNumber + '\'' +
                 '}';
     }
 
