764a9087928a8a342ecf0580790e7599a1b94c40
Elias Baird <eliaseb@stud.ntnu.no>
2021-03-07 13:44:57 +0100

    Oppgave 4 ferdig, lagt til de siste negative testene

--- src/test/java/no/ntnu/eliaseb/DepartmentTest.java
+++ src/test/java/no/ntnu/eliaseb/DepartmentTest.java
@@ -71,14 +71,75 @@ public class DepartmentTest {
             }
             assertFalse(exceptionTriggered);
         }
+
+        @Test
+        public void identicalPersonRemoved(){
+            boolean exceptionTriggered = false;
+            Patient patient = new Patient("Ole", "Nordmann", "123456789");
+            department.addPatient(patient);
+            try {
+                department.remove(new Patient("Ole", "Nordmann", "123456789"));
+            } catch (RemoveException e){
+                exceptionTriggered = true;
+            }
+            assertFalse(exceptionTriggered);
+        }
     }
-
+    @Nested
     class PersonIsNotRemoved{
+        //Jeg prøvd å bruke expetcted exception men det fungerte ikke.
 
-        @Test(expected = IllegalArgumentException.class)
-        public void testExpected() throws IllegalArgumentException
-        {
-            throw new IllegalArgumentException();
+        @Test
+        public void noPersonsInDepartment(){
+            boolean exceptionTriggered = false;
+            Nurse nurse = new Nurse("Ole", "Nordmann", "123456789");
+            try {
+                department.remove(nurse);
+            } catch (RemoveException e){
+                exceptionTriggered = true;
+            }
+            assertTrue(exceptionTriggered);
+        }
+
+        @Test
+        public void wrongPersonInDepartment(){
+            boolean exceptionTriggered = false;
+            Nurse nurse = new Nurse("Ole", "Nordmann", "123456789");
+            Patient patient = new Patient("Ole", "Nordmann", "123456789");
+            department.addPatient(patient);
+            try {
+                department.remove(nurse);
+            } catch (RemoveException e){
+                exceptionTriggered = true;
+            }
+            assertTrue(exceptionTriggered);
+        }
+
+        @Test
+        public void personAlreadyRemoved(){
+            boolean exceptionTriggered = false;
+            Nurse nurse = new Nurse("Ole", "Nordmann", "123456789");
+            department.addEmployee(nurse);
+            try {
+                department.remove(nurse);
+                department.remove(nurse);
+            } catch (RemoveException e){
+                exceptionTriggered = true;
+            }
+            assertTrue(exceptionTriggered);
+        }
+
+        @Test
+        public void personRemovedEqualsNull(){
+            boolean exceptionTriggered = false;
+            Nurse nurse = new Nurse("Ole", "Nordmann", "123456789");
+            department.addEmployee(nurse);
+            try {
+                department.remove(null);
+            } catch (RemoveException e){
+                exceptionTriggered = true;
+            }
+            assertTrue(exceptionTriggered);
         }
     }
 
